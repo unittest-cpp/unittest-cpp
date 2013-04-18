093ef7eca9d1f1651aad2da866fe70bb8cca1786
Christopher Frederick <chris@chrisfrederick.net>
2013-04-18 11:28:43 +0900

    Add a target for TestUnitTest++
    TestUnitTest++ is a command-line tool.

---
+++ xcode_projects/xcode4-6/TestUnitTest++/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TestUnitTest++
+//
+//  Created by Christopher Frederick on 2013/04/18.
+//  Copyright (c) 2013 UnitTest++. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
