3f5095ba13dda9aea740e6085fc8fe37b8ecf81a
Patrick Johnmeyer <pjohnmeyer@gmail.com>
2016-02-04 23:14:42 -0600

    Eliminate some single-line if statements.

--- UnitTest++/ThrowingTestReporter.cpp
+++ UnitTest++/ThrowingTestReporter.cpp
@@ -14,26 +14,38 @@ namespace UnitTest {
    //virtual
    void ThrowingTestReporter::ReportTestStart(TestDetails const& test)
    {
-      if(m_decoratedReporter) m_decoratedReporter->ReportTestStart(test);
+      if(m_decoratedReporter)
+      {
+         m_decoratedReporter->ReportTestStart(test);
+      }
    }
 
    //virtual
    void ThrowingTestReporter::ReportFailure(TestDetails const& test, char const* failure)
    {
-      if(m_decoratedReporter) m_decoratedReporter->ReportFailure(test, failure);
+      if(m_decoratedReporter)
+      {
+         m_decoratedReporter->ReportFailure(test, failure);
+      }
       throw RequiredCheckException();
    }
 
    //virtual
    void ThrowingTestReporter::ReportTestFinish(TestDetails const& test, float secondsElapsed)
    {
-      if(m_decoratedReporter) m_decoratedReporter->ReportTestFinish(test, secondsElapsed);
+      if(m_decoratedReporter)
+      {
+         m_decoratedReporter->ReportTestFinish(test, secondsElapsed);
+      }
    }
 
    //virtual
    void ThrowingTestReporter::ReportSummary(int totalTestCount, int failedTestCount, int failureCount, float secondsElapsed)
    {
-      if(m_decoratedReporter) m_decoratedReporter->ReportSummary(totalTestCount, failedTestCount, failureCount, secondsElapsed);
+      if(m_decoratedReporter)
+      {
+         m_decoratedReporter->ReportSummary(totalTestCount, failedTestCount, failureCount, secondsElapsed);
+      }
    }
 
 }
