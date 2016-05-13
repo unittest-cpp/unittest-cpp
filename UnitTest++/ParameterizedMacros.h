ab3cc22e574973c4d9c57b3b6acf802238109f74
Gabriel Schlozer <g.schlozer@gmail.com>
2016-05-13 22:10:40 +0200

    created macro SET_SUITE_PARAMETERS

---
+++ UnitTest++/ParameterizedMacros.h
@@ -0,0 +1,22 @@
+#ifndef UNITTEST_PARAMETERIZEDMACROS_H
+#define UNITTEST_PARAMETERIZEDMACROS_H
+
+#include "ParameterizedSuite.h"
+
+
+#define SET_SUITE_PARAMETERS(Name, Type) \
+	class ParameterizedCreator ## Name\
+	{ \
+		friend class ParameterizedSuite<## Type>; \
+	public: \
+		ParameterizedCreator ## Name() { create(); } \
+		vector<## Type> parameters; \
+	private: \
+		void create(); \
+	} parameterizedCreator ## Name ## Instance; \
+	\
+	ParameterizedSuite<##Type>  ## Name(UnitTestSuite::GetSuiteName(), parameterizedCreator ## Name ## Instance.parameters); \
+	\
+	void ParameterizedCreator ## Name::create()
+
+#endif
