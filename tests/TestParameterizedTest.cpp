29bb78535837659272ab4f149cba5386ae2b252f
Gabriel Schlozer <g.schlozer@gmail.com>
2016-05-15 02:13:15 +0200

    migrated tests

--- tests/TestParameterizedTest.cpp
+++ tests/TestParameterizedTest.cpp
@@ -1,42 +1,115 @@
 #include "UnitTest++/UnitTest++.h"
 
-#include <string>
 #include "UnitTest++/ParameterizedTest.h"
 
 using namespace std;
 using namespace UnitTest;
 
-SUITE(ParameterizedTest)
+static int simpleValuesSum = 0;
+static int valuesSum = 0;
+static int iterationsSum = 0;
+static int ignoredCounter = 0;
+static int otherParameterizedSuiteSum = 0;
+static int singleValueSuiteSum = 0;
+
+
+SUITE(ParameterizedSuiteSimpl)
 {
-	vector<string> voyelleValues;
-	ParameterizedTest<string> voyelle(voyelleValues);
+	SET_SUITE_PARAMETERS(int, iVal, {
+		parameters.push_back(10);
+		parameters.push_back(20);
+		parameters.push_back(30);
+		parameters.push_back(40);
+	})
 
-	struct InitVoyelle
+	TEST(SimpleSum)
 	{
-		InitVoyelle()
-		{
-			values.push_back("A");
-			values.push_back("E");
-			values.push_back("I");
-			values.push_back("O");
-			values.push_back("U");
-			values.push_back("Y");
-		}
-		vector<string> values;
-	} InitVoyelleInstance;
+		simpleValuesSum += iVal();
+	}
+}
 
-	
-	ParameterizedTest<string> voyelle(InitVoyelleInstance.values);
 
-	
-	static string buildVoyellesStringOutput;
-	TEST(BuildVoyellesString)
+SUITE(ParameterizedSuite)
+{
+	TEST(TestsOfBelowAreIgnored)
 	{
-		buildVoyellesStringOutput += voyelle();
+		ignoredCounter++;
+		CHECK_EQUAL(1, ignoredCounter);
 	}
 
-	TEST(CheckVoyelle)
+	SET_SUITE_PARAMETERS(int, parameterized, {
+		parameters.push_back(1);
+		parameters.push_back(2);
+		parameters.push_back(3);
+		parameters.push_back(4);
+	})
+
+	size_t lastIteration = -1;
+	int lastValue = 0;
+
+	TEST(IterationsAreFollowing)
 	{
-		CHECK_EQUAL("AEIOUY", buildVoyellesStringOutput);
+		CHECK_EQUAL(lastIteration + 1, parameterized.getIteration());
+		lastIteration = parameterized.getIteration();
+		iterationsSum++;
+	}
+
+	TEST(ValuesAreFollowing)
+	{
+		CHECK_EQUAL(lastValue + 1, parameterized());
+		lastValue = parameterized();
+		valuesSum += parameterized();
+	}
+
+	TEST(ValueAndIterationAreSync)
+	{
+		CHECK_EQUAL(parameterized.getIteration() + 1, parameterized());
+	}
+
+
+	SET_SUITE_PARAMETERS(int, parameterized2, {
+		parameters.push_back(1000);
+		parameters.push_back(2000);
+	})
+
+	TEST(OtherPSIgnoredFromFirstPS)
+	{
+		otherParameterizedSuiteSum += parameterized2();
+	}
+}
+
+
+SUITE(ParameterizedSuite_LessValues)
+{
+	SET_SUITE_PARAMETERS(int, parameterizedEmpty, {
+	})
+
+	TEST(WhenNoValue_throwsException)
+	{
+		CHECK_THROW(parameterizedEmpty(), runtime_error);
+	}
+
+	SET_SUITE_PARAMETERS(int, parameterizedSingle, {
+		parameters.push_back(2);
+	})
+
+	TEST(WhenSingleValue_singleExecution)
+	{
+		singleValueSuiteSum += parameterizedSingle();
+		CHECK_EQUAL(2, singleValueSuiteSum);
+	}
+}
+
+
+SUITE(ParameterizedSuite_Validation)
+{
+	TEST(ParameterizedSuiteHasBeenIterated)
+	{
+		CHECK_EQUAL(100, simpleValuesSum);
+		CHECK_EQUAL(1, ignoredCounter);
+		CHECK_EQUAL(10, valuesSum);
+		CHECK_EQUAL(4, iterationsSum);
+		CHECK_EQUAL(3000, otherParameterizedSuiteSum);
+		CHECK_EQUAL(2, singleValueSuiteSum);
 	}
 }
