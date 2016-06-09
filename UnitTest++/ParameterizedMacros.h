1a2908119b55b95e6ea3171aa51991236335f609
Gabriel Schlozer <g.schlozer@gmail.com>
2016-06-09 11:18:44 +0200

    ability to reuse a parameterized test (and "redeclaration error" protection)

--- UnitTest++/ParameterizedMacros.h
+++ UnitTest++/ParameterizedMacros.h
@@ -3,6 +3,12 @@
 
 #include "ParameterizedTest.h"
 
+// For a local usage, use SET_SUITE_PARAMETERS directly in your "myTestSuite.cpp".
+// If you want to reuse a parameterized from an other *.cpp file, use IMPORT_SUITE_PARAMETERS in the new file.
+// If you want to generalize a parameterized (example: "myParameterizedGlobals.h"), use SET_SUITE_PARAMETERS in the *.cpp and IMPORT_SUITE_PARAMETERS in the *.h
+//
+// Warning: please do not use SET_SUITE_PARAMETERS in a *.h file, you might have unexpected duplicate instances !
+
 
 #define SET_SUITE_PARAMETERS(Type, IterationName, SetUpBody) \
 	class ParameterizedCreator ## IterationName\
@@ -13,9 +19,13 @@
 	private: \
 		void create() \
 		## SetUpBody \
-	} static parameterizedCreator ## IterationName ## Instance; \
+	} parameterizedCreator ## IterationName ## Instance; \
 	\
-	static UnitTest::ParameterizedTest<## Type>  ## IterationName(parameterizedCreator ## IterationName ## Instance.parameters, #IterationName);
+	UnitTest::ParameterizedTest<## Type>  ## IterationName(parameterizedCreator ## IterationName ## Instance.parameters, #IterationName);
+
+
+#define IMPORT_SUITE_PARAMETERS(Type, IterationName) \
+	extern UnitTest::ParameterizedTest<## Type>  ## IterationName;
 
 
 #endif
