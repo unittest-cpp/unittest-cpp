1ccb2136d3ab331845bdbfe4f0267f49ad49e00d
Gabriel Schlozer <g.schlozer@gmail.com>
2016-07-01 16:54:19 +0200

    cleaned macro

--- UnitTest++/ParameterizedMacros.h
+++ UnitTest++/ParameterizedMacros.h
@@ -5,21 +5,21 @@
 
 
 #define SET_SUITE_PARAMETERS(Type, Name, SetUpBody) \
-	class ParameterizedCreator ## Name \
+	class ParameterizedCreator##Name \
 	{ \
 	public: \
-		ParameterizedCreator ## Name() : globalInstance(getGlobalInstance()) {} \
-		UnitTest::ParameterizedTest<## Type> & globalInstance; \
+		ParameterizedCreator##Name() : globalInstance(getGlobalInstance()) {} \
+		UnitTest::ParameterizedTest<Type> & globalInstance; \
 	private: \
-		static UnitTest::ParameterizedTest<## Type> & getGlobalInstance() \
+		static UnitTest::ParameterizedTest<Type> & getGlobalInstance() \
 		{ \
-			static UnitTest::ParameterizedTest<## Type> instance(#Name, create()); \
+			static UnitTest::ParameterizedTest<Type> instance(#Name, create()); \
 			return instance; \
 		} \
-		static vector<## Type> create() { vector<## Type> parameters; SetUpBody return parameters; } \
-	} static parameterizedCreator ## Name ## Instance; \
+		static vector<Type> create() { vector<Type> parameters; SetUpBody return parameters; } \
+	} static parameterizedCreator##Name##Instance; \
 	\
-	static UnitTest::ParameterizedTest<## Type> & ## Name(parameterizedCreator ## Name ## Instance.globalInstance)
+	static UnitTest::ParameterizedTest<Type> & Name(parameterizedCreator##Name##Instance.globalInstance)
 
 
 #endif
