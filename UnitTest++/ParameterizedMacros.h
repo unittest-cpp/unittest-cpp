b697f9cc10a5b92fb263ce7a3e7f0f5818714d59
Gabriel Schlozer <g.schlozer@gmail.com>
2016-07-12 14:31:15 +0200

    macro  SET_TEST_PARAMETER_LISTENER(Type, Name, ListenerPtr, SetUpBody)

--- UnitTest++/ParameterizedMacros.h
+++ UnitTest++/ParameterizedMacros.h
@@ -4,7 +4,7 @@
 #include "TestParameter.h"
 
 
-#define SET_TEST_PARAMETER(Type, Name, SetUpBody) \
+#define SET_TEST_PARAMETER_LISTENER(Type, Name, ListenerPtr, SetUpBody) \
 	class ParameterizedCreator##Name \
 	{ \
 	public: \
@@ -13,13 +13,17 @@
 	private: \
 		static UnitTest::TestParameter<Type> & getGlobalInstance() \
 		{ \
-			static UnitTest::TestParameter<Type> instance(#Name, create()); \
+			static UnitTest::TestParameter<Type> instance(#Name, create(), ListenerPtr); \
 			return instance; \
 		} \
 		static vector<Type> create() { vector<Type> values; SetUpBody return values; } \
 	} static parameterizedCreator##Name##Instance; \
 	\
 	static UnitTest::TestParameter<Type> & Name(parameterizedCreator##Name##Instance.globalInstance)
+
+
+#define SET_TEST_PARAMETER(Type, Name, SetUpBody) \
+	SET_TEST_PARAMETER_LISTENER(Type, Name, nullptr, SetUpBody)
 
 
 #endif
