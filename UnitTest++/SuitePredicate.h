2b10f11baf1b2d74a69d974afc96e5705895e2b4
Gabriel Schlozer <g.schlozer@gmail.com>
2016-08-29 23:20:09 +0200

    created predicate "SuitePredicate"

---
+++ UnitTest++/SuitePredicate.h
@@ -0,0 +1,34 @@
+#ifndef UNITTEST_SUITEPREDICATE_H
+#define UNITTEST_SUITEPREDICATE_H
+
+#include <string>
+#include <vector>
+#include "Test.h"
+
+namespace UnitTest
+{
+	using namespace std;
+
+	class UNITTEST_LINKAGE SuitePredicate
+	{
+	public:
+		SuitePredicate();
+		virtual ~SuitePredicate();
+
+		void addSuite(char const* suiteName);
+		void addTest(char const* testName);
+		void addAll();
+
+		bool operator()(Test const * const test) const;
+
+	private:
+		bool mustExecuteSuite(Test const * const test) const;
+		bool mustExecuteTest(Test const * const test) const;
+
+		vector<string> _suiteNames;
+		vector<string> _testNames;
+		bool _all;
+	};
+}
+
+#endif
