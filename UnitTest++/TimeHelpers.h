d288acf1750522c71553ec9141859865b61612d3
paul.bristow <paul@paulcbristow.co.uk>
2017-12-27 07:56:41 +0000

    Add support for Cygwin
    Add UNITTEST_CYGWIN directive and use it to:
    include Posix/TimeHelpers.h for Cygwin,
    exclude the signal handling in SignalTranslator.h/.cpp as Cygwin does not have
    the necessary Posix signal handling support, i.e. sigjmp_buf or siglongjmp.
    
    Reimplement SleepMs in TimeHelpers.cpp using C++11 rather than usleep as that
    is not present on Cygwin.
    
    Update CMakeLists.txt to exclude SignalTranslator.h/.cpp from the build.

--- UnitTest++/TimeHelpers.h
+++ UnitTest++/TimeHelpers.h
@@ -1,6 +1,6 @@
 #include "Config.h"
 
-#if defined UNITTEST_POSIX
+#if defined(UNITTEST_POSIX) || defined(UNITTEST_CYGWIN)
    #include "Posix/TimeHelpers.h"
 #else
    #include "Win32/TimeHelpers.h"
